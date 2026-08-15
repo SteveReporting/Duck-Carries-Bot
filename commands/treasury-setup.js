5b4aa4ad1275f055b8f66aec38ea6a94c50dd44b
David Tennyson <136488774+SteveReporting@users.noreply.github.com>
2026-08-15 14:54:00 +0100

    Add Treasury setup command

---
+++ commands/treasury-setup.js
@@ -0,0 +1,71 @@
+const {
+    ChannelType,
+    MessageFlags,
+    PermissionFlagsBits,
+    SlashCommandBuilder,
+} = require("discord.js");
+
+const {
+    saveSettings,
+    treasuryPanelComponents,
+    treasuryPanelEmbed,
+} = require("../treasury/treasury");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("treasury-setup")
+        .setDescription("Setup The Carry Tavern Treasury")
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+        .addChannelOption((option) =>
+            option
+                .setName("panel")
+                .setDescription("Channel where the Treasury panel will be posted")
+                .addChannelTypes(ChannelType.GuildText)
+                .setRequired(true)
+        )
+        .addChannelOption((option) =>
+            option
+                .setName("category")
+                .setDescription("Category where Treasury tickets will be created")
+                .addChannelTypes(ChannelType.GuildCategory)
+                .setRequired(true)
+        )
+        .addRoleOption((option) =>
+            option
+                .setName("staff-role")
+                .setDescription("Role allowed to manage Treasury tickets")
+                .setRequired(true)
+        )
+        .addChannelOption((option) =>
+            option
+                .setName("logs")
+                .setDescription("Private Treasury log channel")
+                .addChannelTypes(ChannelType.GuildText)
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        const panel = interaction.options.getChannel("panel");
+        const category = interaction.options.getChannel("category");
+        const staffRole = interaction.options.getRole("staff-role");
+        const logs = interaction.options.getChannel("logs");
+
+        saveSettings(
+            interaction.guildId,
+            panel.id,
+            category.id,
+            staffRole.id,
+            logs.id
+        );
+
+        await panel.send({
+            embeds: [treasuryPanelEmbed()],
+            components: treasuryPanelComponents(),
+        });
+
+        return interaction.reply({
+            content: `✅ Treasury configured. Panel posted in ${panel}. Tickets will be created under **${category.name}** and managed by ${staffRole}.`,
+            flags: MessageFlags.Ephemeral,
+        });
+    },
+};
