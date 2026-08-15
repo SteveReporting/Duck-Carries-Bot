21b0f383271f5ab6e0e6dc39f037fb140642e2a9
David Tennyson <136488774+SteveReporting@users.noreply.github.com>
2026-08-15 21:41:51 +0100

    Remove duplicate Treasury interaction listener

--- events/treasuryInteractions.js
+++
@@ -1,9 +0,0 @@
-const { handleTreasuryInteraction } = require("../treasury/treasury");
-
-module.exports = {
-    name: "interactionCreate",
-
-    async execute(interaction) {
-        await handleTreasuryInteraction(interaction);
-    },
-};
