e33d84e74199dec237bb20b170b6855e9c067f34
David Tennyson <136488774+SteveReporting@users.noreply.github.com>
2026-08-26 04:33:06 +0100

    Use clientReady for main startup event

--- events/ready.js
+++ events/ready.js
@@ -56,7 +56,7 @@ async function validateRoleConfiguration(client) {
 }
 
 module.exports = {
-  name: "ready",
+  name: "clientReady",
   async execute(client) {
     console.log(`${client.user.tag} is online`);
     client.user.setActivity("The Carry Tavern 🍺");
